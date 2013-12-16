64e68c34d7d7afcb408d9a2a0ea591652748e7d2
Jestin Stoffel <jestin.stoffel@gmail.com>
2013-12-16 00:50:47 -0600

    updated to match the new naming conventions used in the library

--- src/com/jestinstoffel/sqrldroid/test/SqrlClientTests.java
+++ src/com/jestinstoffel/sqrldroid/test/SqrlClientTests.java
@@ -52,25 +52,25 @@ public class SqrlClientTests extends TestCase {
 	}
 
 	public void testCalculateMasterKey_Succeeds() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		byte[] result = null;
 		try {
-			result = mClient.CalculateMasterKey(new byte[32], "password", new byte[8]);
+			result = mClient.calculateMasterKey(new byte[32], "password", new byte[8]);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mPbkdf).GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
 		assertNotNull(result);
 		assertEquals(32, result.length);
 	}
 	
 	public void testCalculateMasterKey_Bad_MasterIdentityKey_Fails() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		try {
-			byte[] result = mClient.CalculateMasterKey(new byte[31], "password", new byte[8]);
+			byte[] result = mClient.calculateMasterKey(new byte[31], "password", new byte[8]);
 		} catch (Exception e) {
 			assertEquals("master identity key must be 256 bits (32 bytes).", e.getMessage());
 			return;
@@ -80,10 +80,10 @@ public class SqrlClientTests extends TestCase {
 	}
 	
 	public void testCalculateMasterKey_Bad_PBKDF_Output_Fails() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[31]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[31]);
 		
 		try {
-			byte[] result = mClient.CalculateMasterKey(new byte[32], "password", new byte[8]);
+			byte[] result = mClient.calculateMasterKey(new byte[32], "password", new byte[8]);
 		} catch (Exception e) {
 			assertEquals("password key must be 256 bits (32 bytes).  Check validity of PBKDF.", e.getMessage());
 			return;
@@ -93,25 +93,25 @@ public class SqrlClientTests extends TestCase {
 	}
 
 	public void testCalculateMasterIdentityKey_Succeeds() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		byte[] result = null;
 		try {
-			result = mClient.CalculateMasterIdentityKey(new byte[32], "password", new byte[8]);
+			result = mClient.calculateMasterIdentityKey(new byte[32], "password", new byte[8]);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mPbkdf).GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
 		assertNotNull(result);
 		assertEquals(32, result.length);
 	}
 	
 	public void testCalculateMasterIdentityKey_Bad_PBKDF_Output_Fails() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[31]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[31]);
 		
 		try {
-			byte[] result = mClient.CalculateMasterIdentityKey(new byte[32], "password", new byte[8]);
+			byte[] result = mClient.calculateMasterIdentityKey(new byte[32], "password", new byte[8]);
 		} catch (Exception e) {
 			assertEquals("password key must be 256 bits (32 bytes).  Check validity of PBKDF.", e.getMessage());
 			return;
@@ -122,164 +122,164 @@ public class SqrlClientTests extends TestCase {
 
 	public void testGetSqrlDataForLoginByteArrayString_Succeeds() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "sqrl://example.com/auth/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mHmac).GeneratePrivateKey(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).Sign(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).MakePublicKey(Matchers.any(byte[].class));
+		Mockito.verify(mHmac).generatePrivateKey(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).sign(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).makePublicKey(Matchers.any(byte[].class));
 		
 		assertNotNull(result);
-		assertEquals(url.replace("sqrl://", ""), result.Url);
+		assertEquals(url.replace("sqrl://", ""), result.url);
 	}
 
 	public void testCreateIdentity_Succeeds() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
-		Mockito.when(mPbkdf.GetPartialHashFromPasswordKey(Matchers.any(byte[].class))).thenReturn(new byte[16]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mPbkdf.getPartialHashFromPasswordKey(Matchers.any(byte[].class))).thenReturn(new byte[16]);
 		
 		SqrlIdentity result = null;
 		try {
-			result = mClient.CreateIdentity("password", new byte[4096]);
+			result = mClient.createIdentity("password", new byte[4096]);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mPbkdf).GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
-		Mockito.verify(mPbkdf).GetPartialHashFromPasswordKey(Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).getPartialHashFromPasswordKey(Matchers.any(byte[].class));
 		assertNotNull(result);
 	}
 
 	public void testChangePassword_Succeeds() {
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
-		Mockito.when(mPbkdf.GetPartialHashFromPasswordKey(Matchers.any(byte[].class))).thenReturn(new byte[16]);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mPbkdf.getPartialHashFromPasswordKey(Matchers.any(byte[].class))).thenReturn(new byte[16]);
 		
 		SqrlIdentity result = null;
 		try {
-			result = mClient.ChangePassword("oldpassword", new byte[8], "newpassword", new byte[32]);
+			result = mClient.changePassword("oldpassword", new byte[8], "newpassword", new byte[32]);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mPbkdf, Mockito.times(2)).GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
-		Mockito.verify(mPbkdf).GetPartialHashFromPasswordKey(Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf, Mockito.times(2)).generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).getPartialHashFromPasswordKey(Matchers.any(byte[].class));
 		assertNotNull(result);
 	}
 
 	public void testVerifyPassword_Succeeds() {
-		Mockito.when(mPbkdf.VerifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class))).thenReturn(true);
+		Mockito.when(mPbkdf.verifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class))).thenReturn(true);
 		
-		boolean result = mClient.VerifyPassword("password", new SqrlIdentity());
+		boolean result = mClient.verifyPassword("password", new SqrlIdentity());
 		
-		Mockito.verify(mPbkdf).VerifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).verifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class));
 		assertTrue(result);
 	}
 	
 	public void testVerifyPassword_Fails() {
-		Mockito.when(mPbkdf.VerifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class))).thenReturn(false);
+		Mockito.when(mPbkdf.verifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class))).thenReturn(false);
 		
-		boolean result = mClient.VerifyPassword("password", new SqrlIdentity());
+		boolean result = mClient.verifyPassword("password", new SqrlIdentity());
 		
-		Mockito.verify(mPbkdf).VerifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).verifyPassword(Matchers.anyString(), Matchers.any(byte[].class), Matchers.any(byte[].class));
 		assertFalse(result);
 	}
 
 	public void testGetDomainFromUrl_SQRL_Succeeds() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "sqrl://example.com/auth/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mHmac).GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"));
-		Mockito.verify(mSigner).Sign(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).MakePublicKey(Matchers.any(byte[].class));
+		Mockito.verify(mHmac).generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"));
+		Mockito.verify(mSigner).sign(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).makePublicKey(Matchers.any(byte[].class));
 		assertNotNull(result);
-		assertEquals(url.replace("sqrl://", ""), result.Url);
+		assertEquals(url.replace("sqrl://", ""), result.url);
 	}
 	
 	public void testGetDomainFromUrl_QRL_Succeeds() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "qrl://example.com/auth/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mHmac).GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"));
-		Mockito.verify(mSigner).Sign(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).MakePublicKey(Matchers.any(byte[].class));
+		Mockito.verify(mHmac).generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"));
+		Mockito.verify(mSigner).sign(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).makePublicKey(Matchers.any(byte[].class));
 		assertNotNull(result);
-		assertEquals(url.replace("qrl://", ""), result.Url);
+		assertEquals(url.replace("qrl://", ""), result.url);
 	}
 	
 	public void testGetDomainFromUrl_SQRL_Pipe_Succeeds() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "sqrl://example.com/auth|/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mHmac).GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"));
-		Mockito.verify(mSigner).Sign(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).MakePublicKey(Matchers.any(byte[].class));
+		Mockito.verify(mHmac).generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"));
+		Mockito.verify(mSigner).sign(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).makePublicKey(Matchers.any(byte[].class));
 		assertNotNull(result);
-		assertEquals(url.replace("sqrl://", ""), result.Url);
+		assertEquals(url.replace("sqrl://", ""), result.url);
 	}
 	
 	public void testGetDomainFromUrl_QRL_Pipe_Succeeds() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "qrl://example.com/auth|/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 		
-		Mockito.verify(mHmac).GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"));
-		Mockito.verify(mSigner).Sign(Matchers.any(byte[].class), Matchers.anyString());
-		Mockito.verify(mSigner).MakePublicKey(Matchers.any(byte[].class));
+		Mockito.verify(mHmac).generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com/auth"));
+		Mockito.verify(mSigner).sign(Matchers.any(byte[].class), Matchers.anyString());
+		Mockito.verify(mSigner).makePublicKey(Matchers.any(byte[].class));
 		assertNotNull(result);
-		assertEquals(url.replace("qrl://", ""), result.Url);
+		assertEquals(url.replace("qrl://", ""), result.url);
 	}
 	
 	public void testGetDomainFromUrl_Bad_Scheme_Fails() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "http://example.com/auth/asdkjhaiewruhaksdfjiugasdfkjb";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			assertEquals("SQRL urls must begin with 'sqrl://' or 'qrl://'", e.getLocalizedMessage());
 			return;
@@ -290,13 +290,13 @@ public class SqrlClientTests extends TestCase {
 	
 	public void testGetDomainFromUrl_Bad_Url_Fails() throws InvalidKeyException, NoSuchAlgorithmException {
 		String url = "sqrl://example.com";
-		Mockito.when(mHmac.GeneratePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
-		Mockito.when(mSigner.Sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
-		Mockito.when(mSigner.MakePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
+		Mockito.when(mHmac.generatePrivateKey(Matchers.any(byte[].class), Matchers.eq("example.com"))).thenReturn(new byte[32]);
+		Mockito.when(mSigner.sign(Matchers.any(byte[].class), Matchers.anyString())).thenReturn(new byte[64]);
+		Mockito.when(mSigner.makePublicKey(Matchers.any(byte[].class))).thenReturn(new byte[32]);
 		
 		SqrlData result = null;
 		try {
-			result = mClient.GetSqrlDataForLogin(new byte[32], url);
+			result = mClient.getSqrlDataForLogin(new byte[32], url);
 		} catch (Exception e) {
 			assertEquals("SQRL urls must contain a '/'", e.getLocalizedMessage());
 			return;
@@ -342,16 +342,16 @@ public class SqrlClientTests extends TestCase {
 			0x00, 0x00, 0x00, 0x00,
 		};
 
-		Mockito.when(mPbkdf.GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(passwordKey);
+		Mockito.when(mPbkdf.generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class))).thenReturn(passwordKey);
 
 		byte[] result = null;
 		try {
-			result = mClient.CalculateMasterKey(masterIdentityKey, "password", new byte[8]);
+			result = mClient.calculateMasterKey(masterIdentityKey, "password", new byte[8]);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 
-		Mockito.verify(mPbkdf).GeneratePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
+		Mockito.verify(mPbkdf).generatePasswordKey(Matchers.anyString(), Matchers.any(byte[].class));
 		assertEquals(result.length, 32);
 		assertTrue(Arrays.equals(result, masterKey));
 	}
